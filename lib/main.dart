6374c123502996fa75c363afb8d95dc2ff40cc01
nyanky0 <hwngkyo@gmail.com>
2022-06-21 13:12:40 +0700

    perubahan aplikasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 //import 'package:app_pemesaanan_ruang/screen/login_screen.dart';
 import 'package:flutter/material.dart';
-import 'Notifications.dart' as prefix0;
 import 'login_screen.dart';
-import 'SecondPage.dart';
-import 'profil_screen.dart';
-import 'menu_screen.dart';
 
+int _selectedIndex = 0;
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: BottomNav(),
-    theme: appTheme,
-    title: "UAS Rekyasa Areo Yolia Yuda",
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Penyewaan Ruang Universitas Duta Bangsa Surakarta',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: login_screen(
+      ),
+    );
+  }
 }
 
 ThemeData appTheme = ThemeData(
@@ -22,89 +32,3 @@ ThemeData appTheme = ThemeData(
     // fontFamily:
     );
 
-int sel = 0;
-double? width;
-double? height;
-final bodies = [
-  menu_screen(),
-  profil_screen(),
-  login_screen(),
-
-  //prefix0.Notification()
-];
-
-class BottomNav extends StatefulWidget {
-  BottomNav({Key? key}) : super(key: key);
-
-  _BottomNavState createState() => _BottomNavState();
-}
-
-class _BottomNavState extends State<BottomNav> {
-  List<BottomNavigationBarItem> createItems() {
-    List<BottomNavigationBarItem> items = [];
-    items.add(BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.home,
-          color: appTheme.primaryColor,
-        ),
-        icon: Icon(
-          Icons.home,
-          color: Colors.black,
-        ),
-        label: "Home"));
-    items.add(BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.person,
-          color: appTheme.primaryColor,
-        ),
-        icon: Icon(
-          Icons.person,
-          color: Colors.black,
-        ),
-        label: "Profile"));
-    items.add(BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.login,
-          color: appTheme.primaryColor,
-        ),
-        icon: Icon(
-          Icons.login,
-          color: Colors.black,
-        ),
-        label: "Login"));
-    // items.add(BottomNavigationBarItem(
-    //     activeIcon: Icon(
-    //       Icons.notifications,
-    //       color: appTheme.primaryColor,
-    //     ),
-    //     icon: Icon(
-    //       Icons.notifications,
-    //       color: Colors.black,
-    //     ),
-    //p
-    //     label: "Notifications"));
-    return items;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: bodies.elementAt(sel),
-        bottomNavigationBar: BottomNavigationBar(
-          items: createItems(),
-          unselectedItemColor: Colors.black,
-          selectedItemColor: appTheme.primaryColor,
-          type: BottomNavigationBarType.shifting,
-          showUnselectedLabels: false,
-          showSelectedLabels: true,
-          currentIndex: sel,
-          elevation: 1.5,
-          onTap: (int index) {
-            if (index != sel)
-              setState(() {
-                sel = index;
-              });
-          },
-        ));
-  }
-}
